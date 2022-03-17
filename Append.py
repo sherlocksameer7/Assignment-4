56bdbbb086c33ffbf814f4d8842f0c80829de396
sherlocksameer7 <arijitsameers@gmail.com>
2022-03-17 08:07:10 +0530

    append in strings

---
+++ Append.py
@@ -0,0 +1,10 @@
+def name_adder(l1, l2):
+    for i in l1:
+        if len(i) > 0:
+            l2.append(i)
+        return l2
+
+    l1 = ["Sherlock", "Sameer"]
+    l2 = ["Arijit", "Sameer"]
+
+    print(name_adder(l1, l2))
